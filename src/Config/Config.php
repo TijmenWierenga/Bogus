bb4bb971b4abd9bee5c70ee575c1b811c8d279a4
Tijmen Wierenga <t.wierenga@live.nl>
2017-01-31 20:32:27 +0100

    Created Confuguration class + implementation

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -4,5 +4,11 @@ namespace TijmenWierenga\Bogus\Config;
 
 interface Config
 {
-
+    /**
+     * Returns a config value
+     *
+     * @param string $value
+     * @return mixed
+     */
+    public function get(string $value);
 }
--- src/Config/YamlConfig.php
+++ src/Config/YamlConfig.php
@@ -2,12 +2,19 @@
 namespace TijmenWierenga\Bogus\Config;
 
 
+use Symfony\Component\Yaml\Yaml;
+use TijmenWierenga\Bogus\Exception\InvalidArgumentException;
+
+/**
+ * Class YamlConfig
+ * @package TijmenWierenga\Bogus\Config
+ */
 class YamlConfig implements Config
 {
     /**
-     * @var ConfigFile
+     * @var array
      */
-    private $configFile;
+    private $config;
 
     /**
      * YamlConfig constructor.
@@ -15,6 +22,36 @@ class YamlConfig implements Config
      */
     public function __construct(ConfigFile $configFile)
     {
-        $this->configFile = $configFile;
+        $this->parseConfigFile($configFile);
+    }
+
+    /**
+     * @param string $value
+     * @return mixed
+     */
+    public function get(string $value)
+    {
+        $keys = explode('.', $value);
+        $path = $this->config;
+        $x = 0;
+
+        while (array_key_exists($keys[$x], $path)) {
+            $path = $path[$keys[$x]];
+            $x++;
+
+            if ($x === count($keys)) {
+                return $path;
+            }
+        }
+
+        throw new InvalidArgumentException("Non existing config value '{$value}' was requested");
+    }
+
+    /**
+     * @param ConfigFile $configFile
+     */
+    private function parseConfigFile(ConfigFile $configFile): void
+    {
+        $this->config = Yaml::parse(file_get_contents($configFile->getPath()));
     }
 }
