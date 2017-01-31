00b0b15066442e274972751a20d310c4331b136a
Tijmen Wierenga <t.wierenga@live.nl>
2017-01-31 20:33:02 +0100

    Created implementation for mapping files

---
+++ src/Generator/MappingFile/Mappable.php
@@ -0,0 +1,13 @@
+<?php
+namespace TijmenWierenga\Bogus\Generator\MappingFile;
+
+
+use TijmenWierenga\Bogus\Collection\Collection;
+
+interface Mappable
+{
+    /**
+     * @return Collection
+     */
+    public function build(): Collection;
+}
--- src/Generator/MappingFile/MappingFileFactory.php
+++ src/Generator/MappingFile/MappingFileFactory.php
@@ -2,20 +2,68 @@
 namespace TijmenWierenga\Bogus\Generator\MappingFile;
 
 
+use Assert\Assertion;
+use Assert\InvalidArgumentException;
+use TijmenWierenga\Bogus\Collection\BogusCollection;
 use TijmenWierenga\Bogus\Collection\Collection;
+use TijmenWierenga\Bogus\Config\Config;
+use TijmenWierenga\Bogus\Exception\InvalidArgumentException as BogusInvalidArgumentException;
 use TijmenWierenga\Bogus\Generator\Factory;
 
+/**
+ * Class MappingFileFactory
+ * @package TijmenWierenga\Bogus\Generator\MappingFile
+ */
 class MappingFileFactory implements Factory
 {
     /**
-     * MappingFileFactory constructor.
+     * The key where the bindings are registered
      */
-    public function __construct()
+    const MAPPING_CONFIG_KEY = 'mappingFile';
+
+    /**
+     * @var BogusCollection
+     */
+    private $bindings;
+
+    /**
+     * MappingFileFactory constructor.
+     * @param Config $config
+     */
+    public function __construct(Config $config)
     {
+        $this->bindings = new BogusCollection($config->get(self::MAPPING_CONFIG_KEY));
     }
 
-    public function build(string $entityClassName): Collection
+    /**
+     * @param string $entityClassName
+     * @param string $methodName
+     * @return Collection
+     */
+    public function build(string $entityClassName, string $methodName = 'build'): Collection
     {
-        // TODO: Implement build() method.
+        $callable = [$this->getHandler($entityClassName), $methodName];
+        Assertion::isCallable($callable);
+
+        return call_user_func($callable);
+    }
+
+    /**
+     * @param string $entityClassName
+     * @return string
+     */
+    private function getHandler(string $entityClassName): string
+    {
+        try
+        {
+            $handler = $this->bindings->get($entityClassName);
+            Assertion::notNull($handler);
+        }
+        catch (InvalidArgumentException $e)
+        {
+            throw new BogusInvalidArgumentException("No handler was registered for class '{$entityClassName}'");
+        }
+
+        return $handler;
     }
 }
